011adf3dfdfdd1c9df377d6866480e77cbe95fda
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-07-20 09:17:31 +0800

    chore: add hidden admin seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Http\Middleware\Admin;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            AdminSeeder::class,
             ProgramStudiSeeder::class,
             JalurPendaftaranSeeder::class,
             PeriodePendaftaranSeeder::class,
