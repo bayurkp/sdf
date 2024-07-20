07f4a6cd04b978a9c5a94590db5e59152022cef1
Bayu Rizky Kurnia Pratama <bayurkp194@gmail.com>
2024-07-21 05:55:14 +0800

    feat: change order of fields

--- database/migrations/2024_07_18_205745_add_transaction_proof_field_in_users_table.php
+++ database/migrations/2024_07_18_205745_add_transaction_proof_field_in_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->text('bukti_transaksi')->nullable();
+            $table->text('bukti_transaksi')->nullable()->after('prestasi');
         });
     }
 
